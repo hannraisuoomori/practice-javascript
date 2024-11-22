016154cf36405e7c30547e442b6e9bd8091c5c7f
hannraisuoomori <daitoika@icloud.com>
2024-11-22 06:10:15 +0000

    画像を適切に表示

--- RPS.js
+++ RPS.js
@@ -1,8 +1,47 @@
 function RPS(){
-    const choices=["gu","choki","pa","muteki"];
+    const choices=["gu","choki","pa"];
+    let select = document.createElement("select");
+    select.id = "choice"
+    choices.forEach(choice => {
+        let option = document.createElement("option");
+        option.value = choice;
+        option.textContent = choice;
+        option.id = choice;
+        select.appendChild(option);
+    });
+    gameContainer.appendChild(select);
 
+    let choiceButton = document.createElement("button")
+    choiceButton.textContent = "決定"
 
-    // let select = document.createElement("select");
+    gameContainer.appendChild(choiceButton);
+
+    let image = document.createElement("img");
+    image.alt = "choice";
+    image.style.width = "300px";
+    image.style.height = "300px";
+    choiceButton.addEventListener("click",function(){
+    switch(select.value){
+        case "gu":
+            image.src = "https://ddnavi.com/wp-content/uploads/2020/09/1266022.jpg"
+            break;
+        case "choki":
+            image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTevsfZlWoQjuykCSvXeF6cecfuz08nYWajlA&s";
+             break;
+        case "pa":
+            image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRutme5_UiKCN1vLHd2zd3gvqmsjMssNQKGZA&s";
+            break;
+         default:
+            break;
+    }
+    let enemyHand = choices[Math.floor(Math.random()*choices.length)];
+})    
+
+}
+gameContainer.appendChild(image);
+gameContainer.appendChild(enemyImage);
+
+ // let select = document.createElement("select");
     // let option1 = document.createElement("option");
     // let option2 = document.createElement("option");
     // let option3 = document.createElement("option");
@@ -19,21 +58,3 @@ function RPS(){
     // option3.textContent = pa;
     // select.appendChild(option3);
     // gameContainer.appendChild(select);
-
-    let select = document.createElement("select");
-    choices.forEach(choice => {
-        let option = document.createElement("option")
-        option.value = choice;
-        option.textContent = choice;
-        select.appendChild(option);
-    });
-    gameContainer.appendChild(select);
-
-    let image = document.createElement("img")
-    image.alt = "choice";
-    image.style.width = "100px";
-    image.style.height = "900px";
-    image.src = "https://ddnavi.com/wp-content/uploads/2020/09/1266022.jpg"
-
-    gameContainer.appendChild(image);
-}
