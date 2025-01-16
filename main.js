9999c152789f4436d234101eb90493456561e987
hannraisuoomori <daitoika@icloud.com>
2025-01-16 05:18:39 +0000

    ボタンの追加

--- main.js
+++ main.js
@@ -6,6 +6,12 @@
     let button1 = document.createElement("button");
     button1.textContent = "+1ボタン";
 
+    let button2 = document.createElement("button");
+    button2.textContent = "+10ボタン";
+
+    let button3 = document.createElement("button");
+    button3.textContent = "リセットボタン";
+
     let counter = document.createElement("p");
     counter.textContent = count;
 
@@ -14,7 +20,19 @@
         counter.textContent = count;
     })
 
+    button2.addEventListener("click",function(){
+        count = count + 10;
+        counter.textContent = count;
+    })  
+
+    button3.addEventListener("click",function(){
+        count = 0;
+        counter.textContent = count;
+    })  
+
     gameContainer.appendChild(button1);
+    gameContainer.appendChild(button2);
+    gameContainer.appendChild(button3);
     gameContainer.appendChild(counter);
 }
 ClickCounterGame();
